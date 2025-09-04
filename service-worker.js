16f3886cb164db4501b37788b886cb309e50b7ac
Jose Dago <dagoberto.ortiz.g@gmail.com>
2025-09-04 14:26:00 -0500

    Nueva versión PWA con service worker y manifest con nuevo Index

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,71 @@
+// Nombre del caché
 const CACHE_NAME = 'financepro-cache-v1';
+
+// Archivos que se precachean
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icons/icon-72x72.png',
-    './icons/icon-96x96.png',
-    './icons/icon-128x128.png',
-    './icons/icon-144x144.png',
-    './icons/icon-152x152.png',
-    './icons/icon-192x192.png',
-    './icons/icon-384x384.png',
-    './icons/icon-512x512.png',
-    // Archivos de la aplicación principal
-    'https://cdn.tailwindcss.com',
-    'https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js',
-    'https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js'
+  '/',
+  '/index.html',
+  '/styles.css', // Si tuvieras un archivo de estilos separado
+  '/app.js' // Si tuvieras tu lógica de JS en un archivo separado
 ];
 
+// Instalar el Service Worker y almacenar los activos en caché
 self.addEventListener('install', event => {
-    self.skipWaiting();
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Caché abierto con éxito');
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
-});
-
+// Activar el Service Worker
 self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+  event.waitUntil(
+    // Eliminar cachés antiguos
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Eliminando caché antiguo', cacheName);
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
+});
+
+// Interceptar las peticiones de red y servir desde el caché si están disponibles
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Devolver la respuesta desde el caché si se encuentra
+        if (response) {
+          return response;
+        }
+
+        // Si no está en el caché, realizar la petición a la red
+        return fetch(event.request).then(
+          response => {
+            // Comprobar si recibimos una respuesta válida
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clonar la respuesta. Una respuesta es un stream y solo se puede consumir una vez
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
 });
